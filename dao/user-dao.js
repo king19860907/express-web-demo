55e3312ebabe1a19f7af3a9479576ad5b8a6b9fb
king19860907 <ludy19860907@163.com>
2016-03-13 15:16:06 +0800

    增加mongo依赖

---
+++ dao/user-dao.js
@@ -0,0 +1,51 @@
+/**
+ * Created by majun on 16-3-13.
+ */
+var mongodb = require('mongodb');
+
+var dbServer = '192.168.99.100';
+var dbName = 'mydb';
+var collectionName = 'user';
+
+
+var server = new mongodb.Server(dbServer,32774,{auto_reconnect:true});
+var db = new mongodb.Db(dbName,server,{safe:true});
+
+function saveUser(data){
+    db.open(function(err,db){
+        if(!err){
+            db.collection(collectionName,{safe:true},function(err,collection){
+                if(err){
+                    console.log(err);
+                }else{
+                    collection.insertOne(data,{safe:true},function(err,result){
+                        console.log(result);
+                    });
+                }
+            });
+        }else{
+            console.log(err);
+        }
+    });
+}
+
+function findUser(func){
+    db.open(function(err,db){
+        if(!err){
+            db.collection(collectionName,{safe:true},function(err,collection){
+                if(err){
+                    res.send(err);
+                }else{
+                    collection.find().toArray(function(err,docs){
+                       func(docs);
+                    });
+                }
+            });
+        }else{
+            res.send(err);
+        }
+    });
+}
+
+exports.saveUser = saveUser;
+exports.findUser = findUser;
--- routes/mongo.js
+++ routes/mongo.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-var mongodb = require('mongodb');
-var server = new mongodb.Server('10.1.6.210',27001,{auto_reconnect:true});
-var db = new mongodb.Db('mydb',server,{safe:true});
+
+
+var userDao = require('../dao/user-dao');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,40 +11,20 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/insert', function(req, res, next) {
-  db.open(function(err,db){
-    if(!err){
-      db.collection('mycollection',{safe:true},function(err,collection){
-        if(err){
-          console.log(err);
-        }else{
-          var data = {id:1,name:'majun',age:30};
-          collection.insertOne(data,{safe:true},function(err,result){
-            console.log(result);
-          });
-        }
-      });
-    }else{
-      console.log(err);
-    }
-  });
+  var user = {};
+  user.name = 'majun2';
+  user.age = 31;
+  user.id = 2;
+  userDao.saveUser(user);
   res.send('success');
 });
 
 router.get('/find',function(req,res,next){
-  db.open(function(err,db){
-    if(!err){
-      db.collection('mycollection',{safe:true},function(err,collection){
-        if(err){
-          res.send(err);
-        }else{
-          collection.find().toArray(function(err,docs){
-            res.send(docs);
-          });
-        }
-      });
-    }else{
-      res.send(err);
-    }
+  userDao.findUser(function(result){
+    console.log(result);
+    console.log(result[0]);
+    console.log(result[0].id);
+    res.send(result[0].age+'');
   });
 });
 
